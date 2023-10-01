eb7a61d0073e1e2a6dd1b865e652d26218ebd23b
MrMWebDev <tihanymario@gmail.com>
2023-10-01 14:22:09 +0100

    exercise 10

--- myApp.js
+++ myApp.js
@@ -1,23 +1,26 @@
-let express = require('express');
-let app = express();
-let dotenv = require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const dotenv = require("dotenv").config();
 
-console.log("Hello World");
+console.log("Hello Word");
 
-app.use(requestLogger)
+app.use(requestLogger);
+
+app.use(bodyParser);
 
 function requestLogger(req, res, next) {
-    // Get method, path, and IP address from the request object
-    const method = req.method;
-    const path = req.path;
-    const ip = req.ip;
-  
-    // Log the request information
-    console.log(`${method} ${path} - ${ip}`);
-  
-    // Call next() to continue with the request-response cycle
-    next();
-  }
+  // Get method, path, and IP address from the request object
+  const method = req.method;
+  const path = req.path;
+  const ip = req.ip;
+
+  // Log the request information
+  console.log(`${method} ${path} - ${ip}`);
+
+  // Call next() to continue with the request-response cycle
+  next();
+}
 
 // Normal usage
 app.use(express.static(__dirname + "/public"));
@@ -25,38 +28,50 @@ app.use(express.static(__dirname + "/public"));
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get('/json', function(req, res) {
-    // res.json({
-    //     message: "Hello json"
-    //   });
-    let message = 'Hello json';
+app.use(bodyParser.urlencoded({ extended: false }));
 
-    if (process.env.MESSAGE_STYLE === 'uppercase') {
-        message = message.toUpperCase();
-    }
+app.get("/json", function (req, res) {
+  // res.json({
+  //     message: "Hello json"
+  //   });
+  let message = "Hello json";
 
-    res.json({ message });
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+
+  res.json({ message });
 });
 
-app.get("/now", (req, res, next) => {
-      // adding a new property to req object
-      // in the middleware function
-        req.time = new Date().toString();
-        next();
-    },
+app.get(
+  "/now",
+  (req, res, next) => {
+    // adding a new property to req object
+    // in the middleware function
+    req.time = new Date().toString();
+    next();
+  },
 
-    (req, res) => {
-      // accessing the newly added property
-      // in the main function
-        res.send({
-            time: req.time
-        });
-    }
+  (req, res) => {
+    // accessing the newly added property
+    // in the main function
+    res.send({
+      time: req.time,
+    });
+  }
 );
 
-app.get(":/world/echo", req, res => {
-    res.json({ someWorld: req.params.world })
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({
+    echo: word,
+  });
 });
 
+app.get("/name", (req, res) => {
+  res.json({
+    name: req.query.first + " " + req.query.last,
+  });
+});
 
 module.exports = app;
